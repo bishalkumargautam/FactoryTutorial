ad43ad24e458fe554806578157f179efcdce71eb
Bishal Gautam <bishalkrgautam@gmail.com>
2018-09-23 19:04:03 -0500

    Added EmplBO

---
+++ BO/EmplBO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Types;
+
+namespace BO
+{
+    class EmplBO : IEmplBO
+    {
+        private string username;
+        private string password;
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public EmplBO (string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
---
+++ FactoryMethod/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FactoryMethod.Global" Language="C#" %>
